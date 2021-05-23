e24b288b6ee7aa08cd0a7696626cbd0f67a7ba7a
Carlos Tojal <carlos.tojal@hotmail.com>
2021-05-23 13:22:06 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 g++ -c src/Server.cpp -o obj/Server.o
+g++ -c src/StorageObject.cpp -o obj/StorageObject.o
+g++ -c src/Cache.cpp -o obj/Cache.o
 g++ -c src/main.cpp -o obj/main.o
 
-g++ obj/Server.o obj/main.o -o bin/cachedb
+g++ obj/Server.o obj/StorageObject.o obj/Cache.o obj/main.o -o bin/cachedb
--- src/Cache.cpp
+++ src/Cache.cpp
@@ -9,6 +9,8 @@ Cache::Cache() {
 string Cache::get(string key) {
 
   // search for the key in the storage vector and return the value
+
+  return "";
 }
 
 int Cache::set(string key, string value) {
@@ -18,4 +20,6 @@ int Cache::set(string key, string value) {
   // if found, update the value and return 1
 
   // if not found, return 0
+
+  return 0;
 }
